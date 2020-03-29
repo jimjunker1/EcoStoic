519d81713a345e771732f1e383063fa17071c0f5
jimjunker1 <james.junker1@gmail.com>
2020-03-29 14:40:01 -0500

    initial commit

---
+++ install-packages.R
@@ -0,0 +1,11 @@
+# Install pacman if it isn't already installed
+if ("pacman" %in% rownames(installed.packages()) == FALSE) install.packages("pacman")
+
+# Install packages required for analysis
+# Add any packages required for you data cleaning and manipulation to the
+# `p_load` function call below
+# Do not remove the packages already listed here
+# they are important for running the livedat repository
+
+pacman::p_load(tidyverse, git2r, httr, semver, testthat, yaml)
+pacman::p_load_gh("jimjunker1/junkR")
